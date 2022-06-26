56a983900ba080df5ffd6f0320e594b22ddad5e1
baeian <ddoory1103@gmail.com>
2022-06-26 22:15:28 +0900

    0626_1

--- all.c/codeup_1099_2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    int tab[10][10]={};
-    
-
-    return 0;
-}
--- all.c/codeup_1365.c
+++ all.c/codeup_1365.c
@@ -2,30 +2,22 @@
 
 int main()
 {
-    int i,j,n;
-    int nemo[102][102]={};
-    scanf("%d",&n);
-    for ( i = 0; i <= n+1; i++)
-        for ( j = 0; j <= n+1; j++)
-            nemo[i][j]='/';
-    for ( i = 1; i <= n; i++)
-        for ( j = 1; j <= n; j++)
-            nemo[i][j]=' ';
+    int i, j, n, nemo[102][102] = {};
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= n; j++)
+            nemo[i][j] = ' ';
     for (i = 1; i <= n; i++)
     {
-        nemo[1][i]='*';
-        nemo[n][i]='*';
-        nemo[i][1]='*';
-        nemo[i][n]='*';
-        nemo[i][i]='*';
-        nemo[i][n-i+1]='*';
+        nemo[1][i] = '*';
+        nemo[n][i] = '*';
+        nemo[i][1] = '*';
+        nemo[i][n] = '*';
+        nemo[i][i] = '*';
+        nemo[i][n - i + 1] = '*';
     }
-    
-    for ( i = 1; i <= n; i++)
-    {
-        for ( j = 1; j <= n; j++)
-            printf("%c",nemo[i][j]);
-        printf("\n");
-    }
+    for (i = 1; i <= n; i++, puts(""))
+        for (j = 1; j <= n; j++)
+            printf("%c", nemo[i][j]);
     return 0;
 }
