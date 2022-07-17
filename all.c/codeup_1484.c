47774d51fc27af37c864913d84adb6dd0399d840
baeian <ddoory1103@gmail.com>
2022-07-18 02:47:24 +0900

    name

--- all.c/codeup_1484.c
+++ all.c/codeup_1484.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 
+int a[105][105]={1};
+int i,j,x,y,n,m;
+dy[4]={0,1,0,-1}, dx[4]={1,0,-1,0};
+
+safe(int a,int b) {return (0<=a&&a<n)&&(0<=b && b<n);}
 int main()
 {
-    int n,m,str[102][102]={0,},d[4]={1,0,-1,0};
-    scanf("%d%d",&n,&m);
-    for (int i = 1; i <= n; i++)
+    int ny,nx,cnt=1;
+    scanf("%d %d",&n,&m);
+    int s=n*m;
+
+    while (s>cnt)
     {
-        for (int j = 1; j <= m; j++)
+        ny=y+dy[i%4], nx=x+dx[i%4];
+        if (safe(ny,nx) && !a[ny][nx])
         {
-            str[i][j]=1;
+            a[ny][nx]=++cnt;
+            y=ny,x=nx;
         }
+        else i++;    
     }
+
+    for ( i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+            printf("%d",a[i][j]);
+        printf("\n");
+    }
+    return 0;
 }
