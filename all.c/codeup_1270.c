8f0cc8ee4f9652c2e47d630efa9b73148f20c329
baeian <ddoory1103@gmail.com>
2022-07-18 17:17:14 +0900

    1270

---
+++ all.c/codeup_1270.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+   int n, i, a, cnt = 0;
+   scanf("%d", &n);
+   for (i = 1; i <= n; i++)
+   {
+      if (i % 10 == 1)
+         cnt++;
+   }
+   printf("%d", cnt);
+}
