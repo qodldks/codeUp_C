ad5e114a0b1e84ae204be652ccccec1ea7be669d
baeian <ddoory1103@gmail.com>
2022-10-12 18:01:53 +0900

    1205

---
+++ all.c/codeup_1205.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int num1, num2;
+	float temp;
+	scanf("%d %d", &num1, &num2);
+	temp = num1 + num2;
+	if (num1 - num2 > temp)
+		temp = num1 - num2;
+	if (num2 - num1 > temp)
+		temp = num2 - num1;
+	if (num1 * num2 > temp)
+		temp = num1 * num2;
+	if ((float)(num1 / num2) > temp)
+		temp = (float)(num1 / num2);
+	if ((float)(num2 / num1) > temp)
+		temp = (float)(num2 / num1);
+	if (pow(num1, num2) > temp)
+		temp = pow(num1, num2);
+	if (pow(num2, num1) > temp)
+		temp = pow(num2, num1);
+	printf("%.06f", temp);
+	return 0;
+}
