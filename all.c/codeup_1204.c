07f06b5d2fdf561335c46a518ee5843475176562
baeian <ddoory1103@gmail.com>
2022-10-12 18:00:09 +0900

    1204

---
+++ all.c/codeup_1204.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+	int num;
+	scanf("%d", &num);
+	printf("%d", num);
+	if (num % 10 == 1)
+	{
+		if (num == 11)
+			printf("th");
+		else
+			printf("st");
+	}
+	else if (num % 10 == 2)
+	{
+		if (num == 12)
+			printf("th");
+		else
+			printf("nd");
+	}
+	else if (num % 10 == 3)
+	{
+		if (num == 13)
+			printf("th");
+		else
+			printf("rd");
+	}
+	else
+		printf("th");
+
+	return 0;
+}
