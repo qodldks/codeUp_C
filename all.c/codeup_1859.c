5e7c0885d2bcdb6d01fb558b736ae8b2e847274e
baeian <ddoory1103@gmail.com>
2022-11-05 11:10:59 +0900

    1859

---
+++ all.c/codeup_1859.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int arr[30][30], t;
+
+void f(int col, int row)
+{
+	printf("*");
+	if (row == col)
+	{
+		if (col == t - 1)
+			return;
+		printf("\n");
+		f(col + 1, 0);
+	}
+	else
+		f(col, row + 1);
+}
+
+int main()
+{
+	scanf("%d", &t);
+	f(0, 0);
+
+	return 0;
+}
